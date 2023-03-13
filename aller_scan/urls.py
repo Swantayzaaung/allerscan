7d07fe5e66d585d3649565604d6a6fd414fda747
Swantayzaaung <dockyawkyaw@gmail.com>
2023-03-13 20:56:02 +0630

    Created image uploading

--- aller_scan/urls.py
+++ aller_scan/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("scan", views.scan, name="scan"),
-    path("about", views.about, name="about"),
-    path("account", views.account, name="account"),
-    path("login", views.login, name="login"),
-    path("edit", views.edit, name="edit"),
+    path("about", views.about, name="about")
 ]
--- aller_scan/views.py
+++ aller_scan/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import render, HttpResponse
+import os
+from django.core.files.storage import FileSystemStorage 
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib.auth import authenticate, login, logout
-
+from .forms import UploadFileForm
 
 # Create your views here.
 def index(request):
     return render(request, "aller_scan/index.html")
 
+def handle_uploaded_file(f):
+    print(f)
+
 def scan(request):
-    return HttpResponse("scan")
+    if request.method == "POST" and request.FILES['file']:
+        upload = request.FILES['file']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+        file_url = fss.url(file)
+        detect_allergens(file_url)
+        return render(request, "aller_scan/results.html", {
+            'file_url': file_url
+        })
+    else:
+        return render(request, "aller_scan/scan.html")
 
 def about(request):
-    return HttpResponse("about")
-
-def account(request):
-    return HttpResponse("account")
-
-def login(request):
-    return HttpResponse("login")
-
-def edit(request):
-    return HttpResponse("edit")
+    return render(request, "aller_scan/about.html")
